1727dce056ad53373e829604b172c6ada3f68718
BagsMoe <baguzt.abduh@gmail.com>
2025-02-26 05:50:30 +0700

    Tugas Hari-12

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,19 @@
+// import Image from "next/image"
+// import imgBig from "@/assets/bigImg.jpg"
+// import HeavyComponent from "@/components/HeavyComponent"
+import dynamic from "next/dynamic"
+import { useState } from "react"
+
+const HeavyComponent = dynamic(() => import("@/components/HeavyComponent"), { 
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+ })
 export default function About() {
-  return <div>About Page</div>
+  const [show, setShow] = useState(false)
+  return <div>
+    <h1 className="text-3xl font-bold">About Page</h1>
+    <button onClick={() => setShow(true)}>Show Component</button>
+    {/* <img {...imgBig}/> */}
+    {show && <HeavyComponent />}
+  </div>
 }
